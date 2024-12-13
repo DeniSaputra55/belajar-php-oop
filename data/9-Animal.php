eb098c747ded316f9024613682c0f95404314c4b
DeniSaputra <denisaputrafebruari55@gmail.com>
2024-12-13 10:08:07 +0700

    update

--- data/9-Animal.php
+++ data/9-Animal.php
@@ -1,33 +1,40 @@
-<?php 
+<?php
+
 namespace Data;
+
 require_once "10-Food.php";
 //abstract function
-abstract class Animal 
+abstract class Animal
 {
     public string $name;
+
     abstract public function run(): void;
+
     abstract public function eat(AnimalFood $animalFood): void;
 }
-class Cat extends Animal 
+
+class Cat extends Animal
 {
-    public function run(): void 
+    public function run(): void
     {
         echo "Cat $this->name is running" . PHP_EOL;
     }
-    public function eat(AnimalFood $animalFood): void 
+
+    public function eat(AnimalFood $animalFood): void
     {
         echo "Cat is eating" . PHP_EOL;
     }
 }
-class Dog extends Animal 
+
+class Dog extends Animal
 {
-    public function run(): void 
+    public function run(): void
     {
-        echo"Dog $this->name is running" . PHP_EOL;
+        echo "Dog $this->name is running" . PHP_EOL;
     }
-    public function eat(AnimalFood $animalFood): void
+
+    public function eat(Food $animalFood): void
     {
         echo "Dog is eating" . PHP_EOL;
     }
 }
-?>
